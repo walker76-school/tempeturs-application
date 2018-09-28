9cdde74869e0b475ea9780f553d0d561be742df8
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-09-28 14:35:02 -0500

    WIP: Background for home landing screen

--- src/main/js/home/landing.js
+++ src/main/js/home/landing.js
@@ -1,10 +1,33 @@
 import React from 'react';
+import NavItem from 'js/navigation/navitem';
 
 export class Landing extends React.Component {
 	render() {
 		return (
 			<div className="landing">
 
+				<div className="landingNav">
+					<button className="btn" type="register">Register</button>
+					<button className="btn" type="login">Login</button>
+				</div>
+
+				<div className="landingBox">
+					<h1>Tempeturs</h1>
+					<p>A new solution in dog sitting</p>
+					<ol>
+						<li>First</li>
+						<li>Second</li>
+						<li>Third</li>
+					</ol>
+				</div>
+
+				<div className="regBox">
+					<ol>
+						<li>First</li>
+						<li>Second</li>
+						<li>Third</li>
+					</ol>
+				</div>
 			</div>
 		);
 	}
