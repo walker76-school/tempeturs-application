c98ed8b38012f302c0390d10c3b2e69e2663b0a7
Kenna33 <McKenna_Woodrow@baylor.edu>
2018-10-06 00:44:53 -0500

    added calendar files Calendar.js doesn't work only works with redux calendar import

--- src/main/js/account/calendarcomponent.js
+++ src/main/js/account/calendarcomponent.js
@@ -1,26 +1,41 @@
 import React, { Component } from 'react';
 import Calendar from 'react-calendar/dist/entry.nostyle';
+//import Calendar from '/js/account/Calendar';
+//import "./calendar.css";
 
-export default class CalendarComponent extends Component {
+export default class CalendarComponent extends React.Component {
 
-	constructor(props) {
-		super(props);
-		this.onChange = this.onChange.bind(this);
-		this.state = {
-			date: '',
-		};
-	}
 
-	onChange = (date) => {
-		console.log(date);
-		this.setState({ date: date.toString() });
-	};
+    constructor(props) {
+        super(props);
+        this.onChange = this.onChange.bind(this);
+        this.state = {
+            date: '',
+        };
+    }
 
-	render() {
-		return (
-			<div>
-				Calendar
-			</div>
-		);
-	}
+    onChange = (date) => {
+        console.log(date);
+        this.setState({date: date.toString()});
+    };
+
+    render() {
+        return (
+            <div className="App">
+                <header>
+                    <div id="logo">
+                        <span className="icon">date_range</span>
+                        <span>
+                            react<b>calendar</b>
+                        </span>
+                    </div>
+                </header>
+                <main>
+                    <Calendar/>
+                </main>
+            </div>
+        );
+    }
 }
+
+//export default CalendarComponent;
