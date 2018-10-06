e4ba1d0e36a3aedc07618eb37ede80c2b5912a62
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-10-06 12:58:50 -0500

    Adds removing pet - WIP: Confirmation and removal of pet object from database

--- src/main/js/info/petInfo.js
+++ src/main/js/info/petInfo.js
@@ -1,6 +1,8 @@
 import React from 'react';
 import {getPet} from 'js/api/petAPI';
 import {UpdatePetForm} from 'js/forms/updatePetForm';
+import * as Users from 'js/api/usersAPI';
+import {connect} from 'react-redux';
 
 class PetInfo extends React.Component {
 
@@ -9,6 +11,7 @@ class PetInfo extends React.Component {
         this.state = {id: '', name: '', type: '', editing: false};
 		this.setEdit = this.setEdit.bind(this);
 		this.renderForm = this.renderForm.bind(this);
+		this.deletePet = this.deletePet.bind(this);
     }
 
     componentDidMount() {
@@ -24,6 +27,18 @@ class PetInfo extends React.Component {
             alert(error);
         });
     }
+
+    deletePet(){
+		let updatedUser = this.props.user;
+
+		for( let i = 0; i < updatedUser['petIds'].length - 1; i++){
+			if ( updatedUser['petIds'][i] === this.props.petKey) {
+				updatedUser['petIds'].splice(i, 1);
+			}
+		}
+
+		return this.props.updateUser(updatedUser);
+	}
 
     setEdit(){
     	let b = !(this.state.editing);
@@ -43,10 +58,22 @@ class PetInfo extends React.Component {
             <div>
                 <label>Welcome, {this.state.name}! Your id is {this.state.id} and your type is {this.state.type}</label>
 				<button onClick={this.setEdit}>Toggle Edit</button>
+				<button onClick={this.deletePet}>Delete</button>
 				{this.renderForm()}
             </div>
         );
     }
 }
+
+PetInfo = connect(
+	state => ({
+		authentication: Users.State.getAuthentication(state),
+		user: Users.State.getUser(state)
+	}),
+	dispatch => ({
+		updateUser: user => dispatch(Users.Actions.update(user))
+	})
+)(PetInfo);
+
 
 export {PetInfo};
