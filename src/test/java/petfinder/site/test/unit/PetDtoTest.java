2f211688184e26bb3c8ff31016c80716232de122
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-10-10 11:42:46 -0500

    Switches tests over to Junit4

--- src/test/java/petfinder/site/test/unit/PetDtoTest.java
+++ src/test/java/petfinder/site/test/unit/PetDtoTest.java
@@ -1,14 +1,16 @@
 package petfinder.site.test.unit;
 
-import org.junit.jupiter.api.DisplayName;
-import org.junit.jupiter.api.Test;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
 import petfinder.site.common.pet.PetDto;
 
 import java.util.Optional;
 
-import static org.junit.jupiter.api.Assertions.*;
+import static org.junit.Assert.assertEquals;
 
-class PetDtoTest {
+@RunWith(JUnit4.class)
+public class PetDtoTest {
 
     //variables used for testing in later tests
     private PetDto testPetDto = new PetDto(0L, "testPetDto1", "testType1");
@@ -16,17 +18,15 @@ class PetDtoTest {
     private String nameValue = "testPetDto1";
     private String typeValue = "testType1";
 
-    @DisplayName("Testing Constructor Setting Values")
     @Test
-    void constructorTestSetValues(){
+    public void constructorTestSetValues(){
         assertEquals(Optional.ofNullable(testPetDto.getId()), Optional.ofNullable(0L));
         assertEquals(testPetDto.getName(), "testPetDto1");
         assertEquals(testPetDto.getType(), "testType1");
     }
 
-    @DisplayName("Testing Constructor Value Types")
     @Test
-    void constructorTestValueTypes(){
+    public void constructorTestValueTypes(){
 
         assertEquals(testPetDto.getId().getClass(), idValue.getClass());
         assertEquals(testPetDto.getName().getClass(), nameValue.getClass());
