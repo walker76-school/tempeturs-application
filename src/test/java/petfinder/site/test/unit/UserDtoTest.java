dbd8c63074206a198601f6bafa282f4346593af5
BenWin10LenovoFlexx14\Ben_P <ben_pennington@baylor.edu>
2018-10-09 22:33:03 -0600

    Final JUnit Tests for initial constructor values

--- src/test/java/petfinder/site/test/unit/UserDtoTest.java
+++ src/test/java/petfinder/site/test/unit/UserDtoTest.java
@@ -4,10 +4,7 @@ import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import petfinder.site.common.user.UserDto;
 
-import java.util.Arrays;
-import java.util.Collections;
-import java.util.List;
-import java.util.Map;
+import java.util.*;
 
 import static org.junit.jupiter.api.Assertions.*;
 import static petfinder.site.common.user.UserDto.UserType.OWNER;
@@ -15,15 +12,19 @@ import static petfinder.site.common.user.UserDto.UserType.OWNER;
 class UserDtoTest {
 
     //variables used for testing in later tests
-    private UserDto testUserDto1 = new UserDto("testPrincipal", "testName", "testPhoneNumber", Arrays.asList("testRoleInList1", "testRoleInList2"), Arrays.asList(0L, 1L), OWNER, Collections.EMPTY_MAP);
-
     private String testPrincipal1 = "testPrincipal";
     private String testName1 = "testName";
     private String testPhoneNumber1 = "testPhoneNumber";
-    private List<String> testRoles1 = Arrays.asList("testRoleInList1", "testRoleInList2");
-    private List<Long> testPetIds1 = Arrays.asList(0L, 1L);
+    private List<String> testRoles1 = Arrays.asList("testRoleInList1", "testRoleInList2", "testRoleInList3");
+    private List<Long> testPetIds1 = Arrays.asList(0L, 1L, 2L);
     private UserDto.UserType testType1 = OWNER;
-    private Map<String, Object> testAttributes1 = Collections.EMPTY_MAP;
+    private Map<String, Object> testAttributes1 = new HashMap<String, Object>(){{
+        put("testMapAttribute1", new Object());
+        put("testMapAttribute2", new Object());
+        put("testMapAttribute3", new Object());
+    }};
+    private UserDto testUserDto1 = new UserDto("testPrincipal", "testName", "testPhoneNumber", Arrays.asList("testRoleInList1", "testRoleInList2", "testRoleInList3"), Arrays.asList(0L, 1L, 2L), OWNER, testAttributes1);
+
 
     @DisplayName("Testing Constructor Setting Values")
     @Test
@@ -34,7 +35,7 @@ class UserDtoTest {
         assertEquals(testUserDto1.getPhoneNumber(), testPhoneNumber1);
         assertEquals(testUserDto1.getRoles(), testRoles1);
         assertEquals(testUserDto1.getPetIds(), testPetIds1);
-        //assertEquals(testUserDto1.getType(), testType1);
+        assertEquals(testUserDto1.getType(), testType1);
         assertEquals(testUserDto1.getAttributes(), testAttributes1);
     }
 
@@ -47,8 +48,7 @@ class UserDtoTest {
         assertEquals(testUserDto1.getPhoneNumber().getClass(), testPhoneNumber1.getClass());
         assertEquals(testUserDto1.getRoles().getClass(), testRoles1.getClass());
         assertEquals(testUserDto1.getPetIds().getClass(), testPetIds1.getClass());
-        //ssertEquals(testUserDto1.getType().getClass(), testType1.getClass());
+        assertEquals(testUserDto1.getType().getClass(), testType1.getClass());
         assertEquals(testUserDto1.getAttributes().getClass(), testAttributes1.getClass());
     }
-
 }
