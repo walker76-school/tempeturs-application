e409dfc8061add16b01cd31e536336807d104ea7
Andrew Walker <Andrew_Walker2@baylor.edu>
2018-10-16 14:59:05 -0500

    Adds appointment page to display appointments

--- src/main/js/account/components/appointmentComponent.js
+++ src/main/js/account/components/appointmentComponent.js
@@ -1,24 +1,31 @@
 import React, { Component } from 'react';
+import connect from 'react-redux/es/connect/connect';
+import * as Users from 'js/api/usersAPI';
 
 export default class AppointmentComponent extends React.Component {
 
     constructor(props) {
         super(props);
-        this.onChange = this.onChange.bind(this);
-        this.state = {
-            date: '',
-        };
-    }
 
-    onChange = (date) => {
-        this.setState({ date: date.toString() });
-    };
+    }
 
     render() {
         return (
-            <div>
-                Appointment
-            </div>
+			<div>
+				<label>Owner: {this.props.owner}</label>
+				<label>Sitter: {this.props.sitter}</label>
+				<label>Pet: {this.props.petId}</label>
+				<label>Type: {this.props.type}</label>
+				<a>Approve</a>
+			</div>
         );
     }
 }
+
+AppointmentComponent = connect(
+	state => ({
+		user: Users.State.getUser(state),
+	})
+)(AppointmentComponent);
+
+export { AppointmentComponent };
--- src/main/js/account/pages/appointmentPage.js
+++ src/main/js/account/pages/appointmentPage.js
@@ -9,16 +9,25 @@ class AppointmentPage extends React.Component {
 
     constructor(props) {
         super(props);
+		this.state = {
+			appointments: '',
+		};
     }
+
+	componentDidMount(){
+		let component = (<div></div>);
+		if(this.props.user){
+			component = this.props.user.appointments.map((i, index) =>
+				<AppointmentComponent owner={i.owner} sitter={i.sitter} petId={i.petId} type={i.type}/>
+			)
+		}
+	}
     
     render() {
         return (
             <div className='container'>
                 <div className='row'>
-                    <AppointmentComponent/>
-					{_.isDefined(this.props.user) &&
-                        <SitterList zip={this.props.user.zip}/>
-					}
+					{this.state.appointments}
                 </div>
             </div>
         );
