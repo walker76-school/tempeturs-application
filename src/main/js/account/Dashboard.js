133f23f3057353f4b549b425c7fa1c3088e3dae4
Kenna33 <McKenna_Woodrow@baylor.edu>
2018-10-30 17:48:21 -0500

    dashboard has integrated availability page-> no redirection though

--- src/main/js/account/Dashboard.js
+++ src/main/js/account/Dashboard.js
@@ -17,6 +17,7 @@ import NotificationsIcon from '@material-ui/icons/Notifications';
 import { mainListItems, secondaryListItems } from './listItems';
 import SimpleLineChart from './SimpleLineChart';
 import SimpleTable from './SimpleTable';
+import {AvailabilityPage} from 'js/account/pages/availabilityPage';
 
 const drawerWidth = 240;
 
@@ -167,19 +168,8 @@ class Dashboard extends React.Component {
                         <List>{secondaryListItems}</List>
                     </Drawer>
                     <main className={classes.content}>
-                        <div className={classes.appBarSpacer} />
-                        <Typography variant="h4" gutterBottom component="h2">
-                            Orders
-                        </Typography>
-                        <Typography component="div" className={classes.chartContainer}>
-                            <SimpleLineChart />
-                        </Typography>
-                        <Typography variant="h4" gutterBottom component="h2">
-                            Products
-                        </Typography>
-                        <div className={classes.tableContainer}>
-                            <SimpleTable />
-                        </div>
+                        <div className={classes.appBarSpacer}/>
+                        <AvailabilityPage/>
                     </main>
                 </div>
             </React.Fragment>
--- src/main/js/account/listItems.js
+++ src/main/js/account/listItems.js
@@ -9,6 +9,7 @@ import PeopleIcon from '@material-ui/icons/People';
 import BarChartIcon from '@material-ui/icons/BarChart';
 import LayersIcon from '@material-ui/icons/Layers';
 import AssignmentIcon from '@material-ui/icons/Assignment';
+import UpdateUserPage from "js/account/pages/updateUserPage";
 
 export const mainListItems = (
     <div>
