9772d1191723575c9ca6f5b70b68d79b7f6d958a
Lorena Ramirez <lorena_ramirez@baylor.edu>
2018-11-15 17:11:21 -0600

    Updated petDtoTests

--- src/test/java/petfinder/site/test/unit/PetDtoTest.java
+++ src/test/java/petfinder/site/test/unit/PetDtoTest.java
@@ -13,16 +13,18 @@ import static org.junit.Assert.assertEquals;
 public class PetDtoTest {
 
     //variables used for testing in later tests
-    private PetDto testPetDto = new PetDto(0L, "testPetDto1", "testType1");
+    private PetDto testPetDto = new PetDto(0L, "testPetDto1", "testType1", "testBio1");
     private Long idValue = 0L;
     private String nameValue = "testPetDto1";
     private String typeValue = "testType1";
+    private String bioValue = "testBio1";
 
     @Test
     public void constructorTestSetValues(){
         assertEquals(Optional.ofNullable(testPetDto.getId()), Optional.ofNullable(0L));
         assertEquals(testPetDto.getName(), "testPetDto1");
         assertEquals(testPetDto.getType(), "testType1");
+        assertEquals(testPetDto.getBio(), "testBio1");
     }
 
     @Test
@@ -31,6 +33,7 @@ public class PetDtoTest {
         assertEquals(testPetDto.getId().getClass(), idValue.getClass());
         assertEquals(testPetDto.getName().getClass(), nameValue.getClass());
         assertEquals(testPetDto.getType().getClass(), typeValue.getClass());
+        assertEquals(testPetDto.getBio().getClass(), bioValue.getClass());
 
 
     }
