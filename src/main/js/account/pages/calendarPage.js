ca803c3f811eb649af93c36ed113cc1d87ca485e
Kenna33 <McKenna_Woodrow@baylor.edu>
2018-11-15 11:47:24 -0600

    work in progress -> big calendar

--- src/main/js/account/pages/calendarPage.js
+++ src/main/js/account/pages/calendarPage.js
@@ -9,60 +9,20 @@ import TimePickerDialog from 'material-ui/TimePicker/TimePickerDialog';
 //import { withStyles } from '@material-ui/core/styles';
 
 
-class CalendarPage extends React.Component {
+import BigCalendar from 'react-big-calendar'
+import moment from 'moment'
 
-    state = {
-        dateTime: null
-    }
+// Setup the localizer by providing the moment (or globalize) Object
+// to the correct localizer.
+const localizer = BigCalendar.momentLocalizer(moment) // or globalizeLocalizer
 
-    setDate = (dateTime) => this.setState({dateTime})
-
-    render() {
-        return (
-            <MuiThemeProvider>
-                <Calendar
-                    value={new Date()} // picker value moment/string/number/js Date
-                    format='MMM DD, YYYY hh:mm A'
-                    timePickerDelay={150}
-                    returnMomentDate={false} // if true will return moment object
-                    className='datetime-container'
-                    textFieldClassName='datetime-input'
-                    name='picker' // form value name
-                    datePickerMode='landscape' // or landscape
-                    openToYearSelection={false}
-                    disableYearSelection={false}
-                    hideCalendarDate={false}
-                    firstDayOfWeek={1}
-                    minutesStep={1}
-                    showCurrentDateByDefault={false}
-                   // clearIcon={<ClearIcon/>} // set null to not render nothing
-                    // available callbacks
-                    onChange={() => {
-                    }}
-                    onTimePickerShow={() => {
-                    }}
-                    onDatePickerShow={() => {
-                    }}
-                    onDateSelected={() => {
-                    }}
-                    onTimeSelected={() => {
-                    }}
-                    shouldDisableDate={() => {
-                    }}
-                    DatePicker={DatePickerDialog}
-                    TimePicker={TimePickerDialog}
-                    // styles
-
-                    clearIconStyle={{}}
-                    textFieldStyle={{}}
-                    style={{}} // root
-                    timePickerBodyStyle={{}}
-                    fullWidth={true}
-
-                />
-            </MuiThemeProvider>
-        );
-    }
-}
-
-export default (CalendarPage);
+const MyCalendar = props => (
+    <div>
+        <BigCalendar
+            localizer={localizer}
+            events={myEventsList}
+            startAccessor="start"
+            endAccessor="end"
+        />
+    </div>
+);
