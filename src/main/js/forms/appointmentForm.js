14f2ded01b6ed848e46fdf4346adcdb06ed6049a
Kenna33 <McKenna_Woodrow@baylor.edu>
2018-11-15 17:48:43 -0600

    date formatting

--- src/main/js/forms/appointmentForm.js
+++ src/main/js/forms/appointmentForm.js
@@ -44,6 +44,25 @@ const theme = createMuiTheme({
     }
 });
 
+Date.prototype.customFormat = function(formatString){
+    var YYYY,YY,MMMM,MMM,MM,M,DDDD,DDD,DD,D,hhhh,hhh,hh,h,mm,m,ss,s,ampm,AMPM,dMod,th;
+    YY = ((YYYY=this.getFullYear())+"").slice(-2);
+    MM = (M=this.getMonth()+1)<10?('0'+M):M;
+    MMM = (MMMM=["January","February","March","April","May","June","July","August","September","October","November","December"][M-1]).substring(0,3);
+    DD = (D=this.getDate())<10?('0'+D):D;
+    DDD = (DDDD=["Sunday","Monday","Tuesday","Wednesday","Thursday","Friday","Saturday"][this.getDay()]).substring(0,3);
+    th=(D>=10&&D<=20)?'th':((dMod=D%10)==1)?'st':(dMod==2)?'nd':(dMod==3)?'rd':'th';
+    formatString = formatString.replace("#YYYY#",YYYY).replace("#YY#",YY).replace("#MMMM#",MMMM).replace("#MMM#",MMM).replace("#MM#",MM).replace("#M#",M).replace("#DDDD#",DDDD).replace("#DDD#",DDD).replace("#DD#",DD).replace("#D#",D).replace("#th#",th);
+    h=(hhh=this.getHours());
+    if (h==0) h=24;
+    if (h>12) h-=12;
+    hh = h<10?('0'+h):h;
+    hhhh = hhh<10?('0'+hhh):hhh;
+    AMPM=(ampm=hhh<12?'am':'pm').toUpperCase();
+    mm=(m=this.getMinutes())<10?('0'+m):m;
+    ss=(s=this.getSeconds())<10?('0'+s):s;
+    return formatString.replace("#hhhh#",hhhh).replace("#hhh#",hhh).replace("#hh#",hh).replace("#h#",h).replace("#mm#",mm).replace("#m#",m).replace("#ss#",ss).replace("#s#",s).replace("#ampm#",ampm).replace("#AMPM#",AMPM);
+};
 
 class AppointmentForm extends React.Component {
 
@@ -58,8 +77,8 @@ class AppointmentForm extends React.Component {
             endTime: null,
             sitters: false,
             errorCode: 0,
-            dateStartTime: new Date(),
-            dateEndTime: new Date()
+            dateStartTime: new Date().getTime(),
+            dateEndTime: new Date().getTime()
         };
 
         this.addAppointment = this.addAppointment.bind(this);
@@ -70,11 +89,11 @@ class AppointmentForm extends React.Component {
 
 
     setStartDate = (dateTime) => this.setState({
-        dateStartTime: new Date(dateTime)
+        dateStartTime: dateTime
     });
 
     setEndDate = (dateTime) => this.setState({
-        dateEndTime: new Date(dateTime)
+        dateEndTime: dateTime
     });
 
     handleDate(e){
@@ -108,21 +127,19 @@ class AppointmentForm extends React.Component {
             pets: [],
             owner: '',
             sitter: '',
-            date: '',
-            startTime: '',
-            endTime: '',
+            startDate: '',
+            endDate: ''
         };
         appointmentRequest['pets'] = this.state.pets;
         appointmentRequest['owner'] = this.props.user.principal;
         appointmentRequest['sitter'] = principal;
-        appointmentRequest['date'] = this.state.date;
-        appointmentRequest['startTime'] = this.state.startTime;
-        appointmentRequest['endTime'] = this.state.endTime;
+        appointmentRequest['startDate'] = this.state.dateStartTime;
+        appointmentRequest['endDate'] = this.state.dateEndTime;
 
         console.log(appointmentRequest);
 
-        let momentTime = moment(this.state.time);
-        let momentDate = moment(this.state.date);
+        //let momentTime = moment(this.state.time);
+        //let momentDate = moment(this.state.date);
         let renderedDateTime = moment({
             year: momentDate.year(),
             month: momentDate.month(),
@@ -224,8 +241,9 @@ class AppointmentForm extends React.Component {
                 <br/>
 
                 <MuiThemeProvider theme={theme}>
+                    Start Date
                     <DatePicker
-                        value={this.state.dateStartTime} // picker value moment/string/number/js Date
+                        value={(new Date(this.state.dateStartTime)).customFormat( "#DDD# #MMM# #DD#, #YYYY# #hh#:#mm#:#ss# #AMPM#" )} // picker value moment/string/number/js Date
                         format='MMM DD, YYYY HH:mm'
                         timePickerDelay={150}
                         returnMomentDate={true} // if true will return moment object
@@ -238,7 +256,7 @@ class AppointmentForm extends React.Component {
                         hideCalendarDate={false}
                         firstDayOfWeek={1}
                         minutesStep={1}
-                        showCurrentDateByDefault={false}
+                        showCurrentDateByDefault={true}
                         // clearIcon={<ClearIcon/>} // set null to not render nothing
                         // available callbacks
                         onChange={(date) => this.setStartDate(date)}
@@ -260,13 +278,14 @@ class AppointmentForm extends React.Component {
                         textFieldStyle={{}}
                         style={{}}// root
                         timePickerBodyStyle={{}}
-                        fullWidth={true}
+                        fullWidth={false}
                     />
                 </MuiThemeProvider>
 
                 <MuiThemeProvider theme={theme}>
+                    End Date
                     <DatePicker
-                        value={this.state.dateEndTime} // picker value moment/string/number/js Date
+                        value={(new Date(this.state.dateEndTime)).customFormat( "#DDD# #MMM# #DD#, #YYYY# #hh#:#mm#:#ss# #AMPM#" )} // picker value moment/string/number/js Date
                         format='MMM DD, YYYY HH:mm'
                         timePickerDelay={150}
                         returnMomentDate={true} // if true will return moment object
@@ -279,7 +298,7 @@ class AppointmentForm extends React.Component {
                         hideCalendarDate={false}
                         firstDayOfWeek={1}
                         minutesStep={1}
-                        showCurrentDateByDefault={false}
+                        showCurrentDateByDefault={true}
                         // clearIcon={<ClearIcon/>} // set null to not render nothing
                         // available callbacks
                         onChange={(date) => this.setEndDate(date)}
@@ -301,7 +320,7 @@ class AppointmentForm extends React.Component {
                         textFieldStyle={{}}
                         style={{}}// root
                         timePickerBodyStyle={{}}
-                        fullWidth={true}
+                        fullWidth={false}
                     />
                 </MuiThemeProvider>
 
